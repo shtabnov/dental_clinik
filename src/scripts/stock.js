c6e0e736e2bf31452c0bed100c5ac59ba65becac
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-04-27 17:08:00 +0500

    slider ready

---
+++ src/scripts/stock.js
@@ -0,0 +1,30 @@
+new Splide( '.splide', {
+    type: 'loop',
+    perPage: 1,
+    perMove: 1,
+
+    fixedWidth : '410px',
+    fixedHeight: '410px',
+    gap        : '30px',
+
+    breakpoints: {
+		1025: {
+            fixedWidth : '300px',
+            fixedHeight: '300px',
+            gap        : '42px',
+		},
+
+        769: {
+            fixedWidth : '354px',
+            fixedHeight: '354px',
+            gap        : '20px',
+		},
+
+        630: {
+            fixedWidth : '280px',
+            fixedHeight: '280px',
+            gap        : '10px',
+		},
+    }
+
+} ).mount();
